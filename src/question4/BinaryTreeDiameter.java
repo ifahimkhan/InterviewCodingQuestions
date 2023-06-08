518f3575228be56de0cfafc1637f57f2288a9380
ifahimkhan <khan.fahim95@hotmail.com>
2023-06-08 11:44:13 +0530

    This commit will add Question 4:given the root of a  binary tree, return the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree.  This path may or may not pass through the root

---
+++ src/question4/BinaryTreeDiameter.java
@@ -0,0 +1,57 @@
+package question4;
+
+/*given the root of a  binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+This path may or may not pass through the root.*/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BinaryTreeDiameter {
+    private int maxDiameter;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter = 0;
+        calculateDepth(root);
+        return maxDiameter;
+    }
+
+    private int calculateDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftDepth = calculateDepth(node.left);
+        int rightDepth = calculateDepth(node.right);
+
+        // Update the maximum diameter if the sum of left and right depths is greater
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+        // Return the depth of the current node (maximum depth of its left and right subtrees plus 1)
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        BinaryTreeDiameter solution = new BinaryTreeDiameter();
+
+        // Create a binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+
+        int diameter = solution.diameterOfBinaryTree(root);
+        System.out.println("Diameter of the binary tree: " + diameter);
+    }
+}
