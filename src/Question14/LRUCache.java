134945cae515512258902c6ae91e6decaea68d85
ifahimkhan <khan.fahim95@hotmail.com>
2023-06-21 00:13:42 +0530

    This commit will add Question 14:LRU Cache Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.

---
+++ src/Question14/LRUCache.java
@@ -0,0 +1,87 @@
+package Question14;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class LRUCache {
+    private final int capacity;
+    private final Map<Integer, Node> cache;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            removeNode(node);
+            addToFront(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            removeNode(node);
+            addToFront(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node toRemove = tail.prev;
+                removeNode(toRemove);
+                cache.remove(toRemove.key);
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addToFront(newNode);
+        }
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToFront(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));    // Output: 1
+        cache.put(3, 3);
+        System.out.println(cache.get(2));    // Output: -1
+        cache.put(4, 4);
+        System.out.println(cache.get(1));    // Output: -1
+        System.out.println(cache.get(3));    // Output: 3
+        System.out.println(cache.get(4));    // Output: 4
+    }
+}
